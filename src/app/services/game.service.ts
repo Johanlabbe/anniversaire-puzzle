6329ae683e0c9b7f5f11ab01addc64179e2eda96
Johan <labbejohan26@gmail.com>
2026-04-13 06:49:26 +0200

    patch

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -9,18 +9,19 @@ export class GameService {
   public keyParts = signal<string[]>([]);
   
   public fullKey = computed(() => this.keyParts().join('-'));
+// Dans game.service.ts
 
   public finishQuiz(points: number): void {
     this.score.set(points);
-    // Pense à remplacer 'XXXX' par la vraie 1ère partie de ta clé
-    this.keyParts.update(parts => [...parts, 'XXXX']); 
+    // Partie 1 de la fausse clé (L'interrogatoire)
+    this.keyParts.update(parts => [...parts, 'M4Y7H']); 
     this.step.set(2);
   }
 
   public validateCharade(answer: string): boolean {
     if (answer.toLowerCase().trim() === 'discord') {
-      // Pense à remplacer 'YYYY' par la vraie 2ème partie
-      this.keyParts.update(parts => [...parts, 'YYYY']); 
+      // Partie 2 de la fausse clé (La charade)
+      this.keyParts.update(parts => [...parts, 'EF0RC']); 
       this.step.set(3);
       return true;
     }
@@ -29,8 +30,8 @@ export class GameService {
 
   public validateCesar(answer: string): boolean {
     if (answer.toUpperCase().trim() === 'JEDI') {
-      // Pense à remplacer 'ZZZZ' par la vraie 3ème partie
-      this.keyParts.update(parts => [...parts, 'ZZZZ']); 
+      // Partie 3 de la fausse clé (Le code César)
+      this.keyParts.update(parts => [...parts, 'EW17U']); 
       this.step.set(4);
       return true;
     }
