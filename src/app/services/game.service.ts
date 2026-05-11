569ac2f6291740cbcedb7664d5963a3b78d6e616
Johan <labbejohan26@gmail.com>
2026-05-11 06:39:03 +0200

    patch

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -13,15 +13,13 @@ export class GameService {
 
   public finishQuiz(points: number): void {
     this.score.set(points);
-    // Partie 1 de la fausse clé (L'interrogatoire)
-    this.keyParts.update(parts => [...parts, 'M4Y7H']); 
+    this.keyParts.update(parts => [...parts, 'FK5IH']); 
     this.step.set(2);
   }
 
   public validateCharade(answer: string): boolean {
     if (answer.toLowerCase().trim() === 'discord') {
-      // Partie 2 de la fausse clé (La charade)
-      this.keyParts.update(parts => [...parts, 'EF0RC']); 
+      this.keyParts.update(parts => [...parts, 'NGV2T']); 
       this.step.set(3);
       return true;
     }
@@ -30,8 +28,7 @@ export class GameService {
 
   public validateCesar(answer: string): boolean {
     if (answer.toUpperCase().trim() === 'JEDI') {
-      // Partie 3 de la fausse clé (Le code César)
-      this.keyParts.update(parts => [...parts, 'EW17U']); 
+      this.keyParts.update(parts => [...parts, 'MT3X9']); 
       this.step.set(4);
       return true;
     }
